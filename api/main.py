46945d540c99f434d74c340bb07bef179ecaf7e2
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-02-19 22:51:02 +0000

    测试2outh

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 from typing import Union
 from typing import Annotated
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import os
 
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 @app.get("/api/")
@@ -14,15 +15,20 @@ def read_root():
 
 
 @app.post("/api/login/")
-def login():
-    
-    return {"status": "200"}
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    username = os.getenv('LOGIN_USER')
+    if not username == form_data.username:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    password = os.getenv('LOGIN_PWD')
+    if not password == form_data.password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": "OK", "token_type": "bearer"}
+
 
 @app.post("/api/item/")
 def create_item(
-    item: Annotated[
-        Union[str, None],
-        Depends(oauth2_scheme)
-    ]
+    item: Union[str, None],
+    token: str = Depends(oauth2_scheme)
 ):
     return {"item": item}
