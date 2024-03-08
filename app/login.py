c274eea41d0b3b6aa73e1d37bac9741be8762dcc
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-08 06:00:18 +0000

    test

--- app/login.py
+++ app/login.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import os, json
+import os
+import json
 from . import tool, login_tool
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login")
-
 
 
 @router.post("/api/login")
@@ -17,11 +17,18 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     }
     if not userdata in alluserdata:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="用户名或密码错误",
         )
     access_token = login_tool.生成登录令牌(data=userdata)
+    return_data = tool.return_data.copy()
+    return_data.update(
+        {
+            "data": {"access_token": access_token, "token_type": "bearer"}
+        }
+    )
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 @router.get("/api/get_userinfo")
 async def login(token: str = Depends(oauth2_scheme)):
