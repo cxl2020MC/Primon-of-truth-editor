f52f5001b4f3840e2b1c389d6dd427edbacc8852
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-10 05:58:50 +0000

    为失败添加弹窗

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 // dom解析完成后执行
 document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('login-form');
-    const loginErrorMsg = document.getElementById('login-error-msg');
+    const loginForm = document.querySelector('#login-form');
     loginForm.addEventListener('submit', async (e) => {
         e.preventDefault();
         const username = loginForm.username.value;
@@ -18,15 +17,19 @@ document.addEventListener('DOMContentLoaded', () => {
         if (req.ok) {
             const res = await req.json();
             // 处理响应数据
-            const 登录密钥 = res.data.access_token
-            // 保存登录信息
-            window.localStorage.setItem('Auth_Token', 登录密钥);
-            utils.toastify("success", "登录成功");
-            // 跳转到主页
-            setTimeout(() => {
-                window.location.href = '/';
-            }
-            , 1000);
+            if (res.code === 200) {
+                const 登录密钥 = res.data.access_token
+                // 保存登录信息
+                window.localStorage.setItem('Auth_Token', 登录密钥);
+                utils.toastify("success", "登录成功");
+                // 跳转到主页
+                setTimeout(() => {
+                    window.location.href = '/';
+                }, 1000);
+            } else {
+                utils.toastify("error", res.msg);
+            }   
+
         }
     })
 });
