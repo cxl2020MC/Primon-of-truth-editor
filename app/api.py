a380cd03d7c7d1f2d2d4a3a0bc70114779a8c128
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-12 05:53:11 +0000

    test

--- app/api.py
+++ app/api.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, HTTPException, Depends
-from app import db
+from fastapi import APIRouter, HTTPException, Depends, requests
+from app.db import db
 
 router = APIRouter()
 
+@router.get("/api/get_juqin")
+async def 获取剧情(requests):
+    return db.juqin.find()
