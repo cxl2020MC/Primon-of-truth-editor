36f913ff0b0c113bcdbb60be0dccebd8ec75e0f9
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-12 06:01:09 +0000

    test

--- app/api.py
+++ app/api.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends, requests
 from app.db import db
+from app import tool
 
 router = APIRouter()
 
+
 @router.get("/api/get_juqin")
 async def 获取剧情():
+    ret_deta = tool.return_data.copy()
+    ret_deta.update({"data": db.juqin.find()})
     return db.juqin.find()
