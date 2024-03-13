7f9ccd61663812c16097472d1dcc3adf2944688c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-13 23:11:18 +0000

    test

--- app/api.py
+++ app/api.py
@@ -8,6 +8,6 @@ router = APIRouter()
 @router.get("/api/get_juqin")
 async def 获取剧情():
     ret_deta = tool.return_data.copy()
-    ret_deta.update({"data": db.juqin.find()})
+    ret_deta.update({"data": await db.juqin.find().to_list()})
     print(ret_deta)
     return ret_deta
