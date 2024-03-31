7f7109d227304faa1b78996045fe1878c95e1b99
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-03-31 08:36:33 +0000

    test

--- app/api.py
+++ app/api.py
@@ -10,7 +10,7 @@ router = APIRouter()
 async def 获取剧情():
     ret_deta = tool.return_data.copy()
     juqinnames = db.jqnames.find({})
-    jqnamedata = await juqinnames.to_list(length=100)
+    jqnamedata = juqinnames.to_list(length=100)
     ret_deta.update({"data": jqnamedata})
     print(ret_deta)
     return ret_deta
@@ -20,8 +20,8 @@ async def 获取剧情():
 async def 保存剧情(name: str, data: dict):
     print(data)
     if name:
-        await db.jqnames.insert_one({"name": name})
-        await db.jqdata.insert_one({
+        db.jqnames.insert_one({"name": name})
+        db.jqdata.insert_one({
             "name": name,
             "data": data
         })
--- app/db.py
+++ app/db.py
@@ -1,7 +1,9 @@
-import motor.motor_asyncio
+# import motor.motor_asyncio
+import pymongo
 import os
 
 数据库地址 = os.getenv("MONGODB_URL")
-client = motor.motor_asyncio.AsyncIOMotorClient(数据库地址)
+# client = motor.motor_asyncio.AsyncIOMotorClient(数据库地址)
+client = pymongo.MongoClient(数据库地址)
 
 db = client.tofe
