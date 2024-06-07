c77e17e731a8291479e1ed3a81614cae542da5b9
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-06-07 07:50:18 +0000

    test

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 # from pydantic import BaseModel
 from typing import List
-from core import tool
+import traceback
+
+from core import tool, login, api
+
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+app.include_router(login.router)
+app.include_router(api.router)
+
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
     retdeta = tool.return_data
 
-    return retdeta.update(status=exc.status_code, msg=exc.detail)
+    retdeta.update(status=exc.status_code, msg=traceback.format_exc())
+
+    return retdeta
     
 
 if __name__ == "__main__":
